262cfdf70a7ef162f175bbbc2b53b27695c291c6
vaishnavi varaganti <vaishnavivaraganti25@gmail.com>
2023-10-19 15:42:19 -0500

    added code to check if the request has query arguement in request in computation endpoint

--- routes/computation.js
+++ routes/computation.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var randomValue = Math.random();
+var randomValue;
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    randomValue = Math.random();
+    var x = req.query.x;
+    if (x !== undefined) {
+        randomValue = parseFloat(x);
+    }
+    else {
+        randomValue = Math.random();
+    }
     res.send(`Math.fround applied to ${randomValue} is ` + Math.fround(randomValue) + '<br>' +
         `Math.random is ` + Math.random() + '<br>' +
         `Math.round applied to ${randomValue} is ` + Math.round(randomValue) + '<br>');
