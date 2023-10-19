251b4139de9e1d6688776dcd04f10b8a094492e6
vaishnavi varaganti <vaishnavivaraganti25@gmail.com>
2023-10-19 15:17:51 -0500

    modified the users endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+var sum = 0;
+var more = 0;
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  more = more + 1;
+  sum += more;
+  res.send(`Sum is: ${sum}`);
 });
 
 module.exports = router;
